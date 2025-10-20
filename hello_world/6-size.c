d5fab0b3fa1c4a64056c11c5ec770229dab62385
youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-20 14:16:14 +0200

    add 5-printf.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of integer: %d\n",sizeof(int));
+	printf("Size of long integer: %d\n",sizeof(long));
+	printf("Size of float: %d\n",sizeof(float));
+	printf("Size of char: %d\n",sizeof(char));
+	printf("Size of double: %d\n",sizeof(double))
+    return (0);
