0301f6db6fb872c96b6c3c25cef1edc538dabdd1
youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-20 11:58:44 +0200

    add 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* include stdio.h is to used to include the standard input-output library in c */
 /*int main(void) is the function of our programm that runs without declaring variables*/
-/*return (0) is to indicate that our programs run succesfuly wihout error 
+/*return (0) is to indicate that our programs run succesfuly wihout error*/ 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
