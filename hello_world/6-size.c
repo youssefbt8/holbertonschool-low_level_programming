48ab1175364467b7956df3915e93a8e571d23af6
youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-20 14:21:10 +0200

    add 6-size.C

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of float: %d\n",sizeof(float));
 	printf("Size of char: %d\n",sizeof(char));
 	printf("Size of double: %d\n",sizeof(double));
-    return (0);
+	return (0);
 }
