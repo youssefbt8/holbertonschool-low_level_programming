c491717a7588437bdef48adbaa206480300b29a9
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-21 10:40:48 +0200

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
     n = rand() - RAND_MAX / 2;
     last_digit = n % 10;
     if (last_digit < 0)
-        last_digit = -last_digit;  // Make last_digit positive
+        last_digit = -last_digit;
 
     printf("Last digit of %d is %d", n, last_digit);
 
