01fdcd6d84c272f3fb731107d1bdbd8708734dad
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-21 13:40:14 +0200

    add 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 	char CH = 'A';
 
-        while (ch <= 'z')
-        {
-        putchar(ch);
-        ch++;
-        }
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	ch++;
+	}
 
 	while (CH <= 'Z')
 	{
