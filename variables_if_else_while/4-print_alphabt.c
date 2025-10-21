6393fb9a4d7356beae7a99de5f0bca56ae8b233d
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-21 13:56:36 +0200

    add 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	char ch = 'a';
-	
+
 	while (ch <= 'z')
 	{
-	if (ch != 'q' && ch!= 'e')
+	if (ch != 'q' && ch != 'e')
 	putchar(ch);
 	ch++;
-	}	
+	}
 	putchar('\n');
 	return (0);
 }
