d0e6a364591fa39e10a58d4b1e471d795a8f06eb
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-21 14:56:52 +0200

    add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+	
+	while (number < 16)
+	{
+	if (number < 10)
+	putchar(number + '0');
+	else
+	putchar(number + 'a' - 10);
+	}
+	return (0);
+}
