2b706c455b9e03ed07e894bebb22808cac3a6584
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-22 12:20:34 +0200

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-	putchar("/_putchar");
+	putchar("/_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
 	return (0);
 }
