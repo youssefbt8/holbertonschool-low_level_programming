48f7eff1f5fbefaddea4d5b5854b079e4dc20165
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-22 16:16:52 +0200

    add 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  */
 void print_alphabet(void)
 {
-    char c = 'a';
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
-    }
-    _putchar('\n');
+	char c = 'a';
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
 }
