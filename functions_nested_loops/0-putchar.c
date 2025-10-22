5d0490069639f06dd46099f83d8d6fd3351eb546
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-22 13:08:13 +0200

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@ int _putchar(char c);
  */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i;
+	char str[] = "_putchar\n";
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+	_putchar(str[i]);
 
-    return (0);
+	return (0);
 }
 
