2d69c5e1072dbe518351c6b7dfadce6d0695f626
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-23 13:37:32 +0200

    add 7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code that is in the function
+ * @n:the number given
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int ld)
+{
+	int n
+	ld = n % 10
+	if (ld <= 0)
+	{
+	ld = -ld
+	else
+	ld = ld
+	_putchar('0' + ld)
+	}
+	return (0)
+}
