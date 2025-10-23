dcecb5ca12b0d689c8ec47d52fbabbfe1f01a47a
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-23 09:42:41 +0200

    add 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-    int i = 0;
-    char c;
+	int i = 0;
+	char c;
 
-    while (i < 10)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-        i++;
-    }
+	while (i < 10)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
