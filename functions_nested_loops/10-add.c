5029d4f28762f9d70a17d99a593580a32f16936a
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-26 14:56:04 +0100

    add 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * add.c - print the result of adding two integers
+ * @a:number to give first
+ * @b:number to give second
+ * return:always 0
+*/
+int add(int a, int b)
+{
+	return(a + b)
+}
