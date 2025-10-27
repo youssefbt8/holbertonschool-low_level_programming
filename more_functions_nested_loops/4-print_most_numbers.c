ff5e1ba9ae7bbbce00095b93e095a86a7a811f90
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-27 11:21:21 +0100

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number from 0 to 9 without 2 and 4
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+	if (n != 2 && n != 4)
+        _putchar(n);
+	}
+        _putchar('\n');
+}
