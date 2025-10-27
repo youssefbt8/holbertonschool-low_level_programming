43657802f09c44c690eaa661f9f6dfbc2dc88afd
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-27 14:03:49 +0100

    add 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
- *@n : numbers of straight line 
+ *@n:numbers of straight line
  *
  *Return:void
  */
@@ -19,4 +19,4 @@ void print_line(int n)
 	_putchar('_');
 	}
 	_putchar('\n');
-}	
+}
