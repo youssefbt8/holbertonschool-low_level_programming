a9d6ec986f73980fd703a05bc8822c54454a2646
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-29 14:02:35 +0100

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,17 +8,18 @@ void rev_string(char *s)
 {
 	int l = 0;
 
+	int m = l / 2;
+
 	while (s[l] != '\0')
 	{
 	l++;
 	}
-	int m = l / 2;
-	char  tmp;
+	char tmp;
 	int i = 0;
 	for (i = 0 ; i < m ; i++)
 	{
 	tmp = s[i];
-	s[i] = s[l - 1 - i]
-	s[l - 1 - i] = tmp
+	s[i] = s[l - 1 - i];
+	s[l - 1 - i] = tmp;
 	}
 }
