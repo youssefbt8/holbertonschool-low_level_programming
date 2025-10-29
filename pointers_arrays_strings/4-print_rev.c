14a56f72040b0257569540dad68dff0766b4327c
Youssefbt8 <youcefboubtane2004@gmail.com>
2025-10-29 11:22:14 +0100

    4-print_rev.c add

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s:pointer to the string
+ *
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (l = l - 1 ; l >= 0 ;l--)
+	{
+	_putchar(s[l]);
+	}
+	_putchar('\n');
+}
